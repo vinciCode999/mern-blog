0f500527532861fba7f3374cc6613424d90772d0
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2024-07-31 00:13:49 +0200

    create sign up page ui

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -12,8 +12,8 @@ export default function Header() {
         <span className='px-2 py-1 
         bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg
         text-white
-        '>RESNECT</span>
-        VIDA
+        '>Alerta</span>
+        VIVA
       </Link>
 
       <form>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,71 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div className='min-h-screen mt-20'>
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row
+        md:items-center gap-5
+      ">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className='font-bold
+          dark:text-white text-4xl'>
+            <span className='px-2 py-1 
+            bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg
+            text-white
+          '>Alerta</span>
+          Viva
+        </Link>
+        <p className='text-sm mt-5'>
+          Mantente seguro con AlertaViva. 
+          Regístrate para recibir alertas en tiempo real y actualizaciones esenciales adaptadas a tu área. 
+          Mantente informado y preparado durante emergencias con nuestra información oportuna. 
+          Regístrate ahora y mantente conectado cuando más lo necesites.
+        </p>
+      </div>
+
+        {/* right */}
+        <div className="flex-1">
+          <form className='flex  flex-col gap-4'>
+            <div>
+              <Label value="Your Username"/>
+              <TextInput
+                type="text"
+                id="username"
+                placeholder='username'
+              /> 
+            </div>
+
+            <div>
+              <Label value="Your Email"/>
+              <TextInput 
+                type='email'
+                placeholder='name@company.com'
+                id="email"
+              /> 
+            </div>
+
+            <div>
+              <Label value="Your Password"/>
+              <TextInput
+                type="password"
+                placeholder='password'
+                id="password"
+              /> 
+            </div>
+            <Button gradientDuoTone='purpleToPink'>
+              Sign Up
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-4'>
+            <span className='text-sm text-gray-500'>You have an accont?</span>
+            <Link className='text-blue-500' to="/sign-in">Sign In</Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
