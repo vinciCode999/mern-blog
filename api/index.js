9134699e3ef7a56c713baa738ee14aa22934b80e
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2024-08-01 20:04:09 +0200

    create test api

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 dotenv.config()
 
 
 const app = express();
 const port = process.env.PORT || 3000; // Default to port 3000 if PORT is not defined
 const mongodbUri = process.env.MONGODBURI;
+app.use('/api/user',userRouter)
 
 mongoose.connect(mongodbUri)
   .then(()=>{
